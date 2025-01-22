e026a2d1889642bc3cedb3c7a298a74fa96411d1
Chazelam <116812223+Chazelam@users.noreply.github.com>
2025-01-22 13:50:40 +0300

    Update keys.py

--- bot/keys.py
+++ bot/keys.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '7399597593:AAGq0lPqpJtf3iR4HN8X5tabmsD-CE7KKuw'
+BOT_TOKEN = ''
 dataBaseName = "base.json"
 elements = {"1": "Glacio", "2": "Fusion", "3" :"Electro", "4": "Aero", "5": "Spectro", "6": "Havoc"}
